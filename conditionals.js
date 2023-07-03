fcdbb3d396ad4f04b85aa5f1d684da9b88417816
AkhmadZiyadAthallah <akhmd.zyd2008@gmail.com>
2023-07-03 15:35:02 +0800

    Conditionals.js

--- conditionals.js
+++ conditionals.js
@@ -2,12 +2,12 @@
 // kemudian tampilkan pesan apakah mereka telah mencapai lebih dari 15 tahun jika kurang dari 15 tahun keluarkan pesan
 // umur anda lebih kecil dari 15 tahun
 
-var age;
+var age = 17
 // isilah bagian if dengan kondisi jika age > 15 tahun maka cetak anda berumur diatas 15 tahun  
-if(age){
-
+if(age >= 15) {
+    console.log("Anda sudah 15 tahun atau pun lebih!")
 }
 // jika tidak ditemukan maka cetak anda berumur kurang dari 16 tahun
-else{
-    
+else {
+    console.log("Anda belum mencapai 15 tahun!")
 }
